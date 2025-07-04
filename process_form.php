7b4fa048d97660cc2b810f8abe7a15d97f6b5526
Horizon-Tech <massenajonas256@gmail.com>
2025-07-04 13:49:02 -0400

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,20 +1,20 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nom = $_POST["nom"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+    $nom = htmlspecialchars(trim($_POST["name"])); // "name" ici correspond à name="name"
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = htmlspecialchars(trim($_POST["message"]));
 
     $destinataire = "massenajonas256@gmail.com";
     $sujet = "Nouveau message du formulaire de contact";
 
-    $contenu = "Nom: $name\n";
+    $contenu = "Nom: $nom\n";
     $contenu .= "Email: $email\n";
     $contenu .= "Message:\n$message";
 
-    $headers = "From: $name <$email>";
+    $headers = "From: $nom <$email>";
 
     if (mail($destinataire, $sujet, $contenu, $headers)) {
-        echo "Nos Félicitacions!!! Votre message a été envoyé avec succès.";
+        echo "Nos Félicitations !!! Votre message a été envoyé avec succès.";
     } else {
         echo "Une erreur s'est produite lors de l'envoi du message.";
     }
